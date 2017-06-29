3a7779902be69205fd3aa0ead0e1b40c19d23986
VaibhavBajaj <vaibhav_bajaj2005@yahoo.com>
2017-06-28 23:47:20 -0700

    Completed database architecture

--- constants.py
+++ constants.py
@@ -1,23 +1,60 @@
 from secretConstants import PASSWORD
+from secretConstants import DB_NAME
 
 USERNAME = 'root'
 HOST_CONFIG = '127.0.0.1'
-DB_NAME = 'yonyx_project'
 
-TABLE_TEMPLATE = (
-    "  CREATE TABLE {0} ("
-    "'To' DATE NOT NULL,"
-    "'CustomerName' VARCHAR(40) NOT NULL,"
-    "'InternalUsers' INT(11) NOT NULL,"
-    "'Valid' BOOLEAN NOT NULL"
-    ") ENGINE=InnoDB"
+TABLE_TEMPLATE = dict()
+TABLE_TEMPLATE['Directory'] = (
+    "CREATE TABLE Directory ("
+    "  CustomerName VARCHAR(40) NOT NULL,"
+    "  Subdomain VARCHAR(100) NOT NULL,"
+    "  Valid BOOLEAN NOT NULL,"
+    "  PRIMARY KEY (Subdomain)"
+    ");"
+)
+TABLE_TEMPLATE['Metadata'] = (
+    "CREATE TABLE Metadata ("
+    "  Id INTEGER(11) NOT NULL AUTO_INCREMENT,"
+    "  ToDate DATE NOT NULL,"      # Please see that "To_Date" is different from the excel field "To"
+    "  InternalUsers INTEGER(11) NOT NULL,"
+    "  Subdomain VARCHAR(100) NOT NULL,"
+    "  PRIMARY KEY (Id)"
+    ");"
 )
 
-PAYLOAD_TEMPLATE = (
-    "INSERT INTO {0}"
-    "(to_date, customer_name, internal_users, validity)"
-    "VALUES (%s, %s, %s, %s)"
+COMMANDS = dict()
+COMMANDS['Check_Entry_Existence_Directory'] = (
+    "SELECT EXISTS("
+    "SELECT * "
+    "FROM Directory "
+    "WHERE Subdomain = \"{0}\""
+    ");"
 )
+COMMANDS['Check_Entry_Existence_Metadata'] = (
+    "SELECT EXISTS("
+    "SELECT * "
+    "FROM Metadata "
+    "WHERE Subdomain = \"{0}\" AND ToDate = \"{1}\""
+    ");"
+)
+
+COMMANDS['Insert_Directory'] = (
+    "INSERT INTO Directory "
+    "(CustomerName, Subdomain, Valid) "
+    "VALUES (%s, %s, %s)"
+)
+COMMANDS['Insert_Metadata'] = (
+    "INSERT INTO Metadata "
+    "(ToDate, InternalUsers, Subdomain) "
+    "VALUES (%s, %s, %s)"
+)
+COMMANDS['Update_Validity_Directory'] = (
+    "UPDATE Directory "
+    "SET Valid = 0 "
+    "WHERE Subdomain = \"{0}\""
+)
+
 
 config = {
     'user': USERNAME,
--- parseExcel.py
+++ parseExcel.py
@@ -2,23 +2,24 @@ import csv
 import mysql.connector
 from constants import config
 from constants import TABLE_TEMPLATE
-from constants import PAYLOAD_TEMPLATE
+from constants import COMMANDS
 from secretConstants import EXCEL_PATH
 from mysql.connector import errorcode
 from datetime import datetime
 
 
-def create_table(dataDict):
-    try:
-        print("Creating table {}: ".format(dataDict['Subdomain']), end='')
-        cursor.execute(TABLE_TEMPLATE.format(dataDict['Subdomain']))
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-            print("already exists.")
+def create_table():
+    for name, cmd in TABLE_TEMPLATE.items():
+        try:
+                print("Creating table {0}: ".format(name))
+                cursor.execute(cmd)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
         else:
-            print(err.msg)
-    else:
-        print("OK")
+            print("OK")
 
 # Opening mysql connection
 try:
@@ -33,23 +34,67 @@ except mysql.connector.Error as err:
         print(err)
 else:
     # Yipee! We connected
-    cursor = connection.cursor()
+    print("MySQL connection successful.")
+    cursor = connection.cursor(buffered = True)
 
     # Opening csv table to feed data
     with open(EXCEL_PATH, 'r') as weeklyInsight:
-        reader = csv.DictReader(weeklyInsight)
-        for dataDict in reader:
-            # Creates table if not previously existing
-            create_table(dataDict)
+        # List of dictionaries with data returned
+        dictCollection = csv.DictReader(weeklyInsight)
+        # Creates table if not previously existing
+        create_table()
 
+        for dictElement in dictCollection:
             # Changing date to %m/%d/%Y format
-            to_date = dataDict['To'][:5] + "20" + dataDict['To'][5:]
-            validity = False if dataDict['Disabled Today'] is 'Yes' else True
-            payload_data = (
-                datetime.strptime(to_date, '%m/%d/%Y'),
-                dataDict['CustomerName'],
-                dataDict['InternalUsers'],
-                validity
+            to_date = dictElement['To'][:5] + "20" + dictElement['To'][5:]
+            to_date = datetime.strptime(to_date, '%m/%d/%Y')
+            metadata_payload = (
+                datetime.strftime(to_date, '%Y-%m-%d'),
+                dictElement['InternalUsers'],
+                dictElement['Subdomain']
             )
-            cursor.execute(PAYLOAD_TEMPLATE.format(dataDict['Subdomain']), payload_data)
+
+            # Check existence of metadata in Table Metadata
+            cursor.execute(COMMANDS['Check_Entry_Existence_Metadata']
+                           .format(dictElement['Subdomain'], datetime.strftime(to_date, '%Y-%m-%d')))
+
+            # When debugging, do not print fetchone().
+            # If called more than once after cursor.execute(), its value becomes None.
+            if cursor.fetchone()[0] == 0:
+                # Metadata doesn't already exist in database
+                # Insert metadata into table Metadata
+                try:
+                    cursor.execute(COMMANDS['Insert_Metadata'], metadata_payload)
+                except mysql.connector.Error as err:
+                    print("Error in inserting metadata: {0}".format(err.msg))
+
+            # Check existence of directory in Table Directory
+            cursor.execute(COMMANDS['Check_Entry_Existence_Directory']
+                           .format(dictElement['Subdomain']))
+
+            # Checking company contract validity.
+            valid = 0 if dictElement['Disabled Today'] == 'Yes' else 1
+
+            if cursor.fetchone()[0] == 0:
+                directory_payload = (
+                    dictElement['CustomerName'],
+                    dictElement['Subdomain'],
+                    valid
+                )
+                try:
+                    cursor.execute(COMMANDS['Insert_Directory'], directory_payload)
+                except mysql.connector.Error as err:
+                    print("Error in inserting directory: {0}".format(err.msg))
+            else:
+                # Subdomain directory already exists
+                if valid == 0:
+                    # If subdomain directory exists and validity has become False
+                    # (only happens once), update validity of existing directory to False
+                    cursor.execute(COMMANDS['Update_Validity_Directory']
+                                   .format(dictElement['Subdomain']))
+
+    # Committing data added
+    connection.commit()
+
+    cursor.close()
     connection.close()
